a2ad0b0b43b2e83543497fc84f9c035fe5d30e29
jame10000lnw <38467489+jame10000lnw@users.noreply.github.com>
2021-05-07 11:10:37 +0700

    Update version2.php

--- version2.php
+++ version2.php
@@ -1,5 +1,5 @@
 {
-"Version":"1.9",
+"Version":"1.10",
 "Url":"https://github.com/jame10000lnw/updata/raw/main/ok.zip",
 "Changelog":"--------------------------------------------------
 🔰 LIFESTYLE VPN🔰
