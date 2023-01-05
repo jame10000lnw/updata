aea6b05e09a89a9b9a3acb10aa04ac545be1a896
jame10000lnw <38467489+jame10000lnw@users.noreply.github.com>
2023-01-05 20:53:34 +0700

    Update updateappvpnkubvpn.php

--- updateappvpnkubvpn.php
+++ updateappvpnkubvpn.php
@@ -1,1 +1,7 @@
-
+{
+  
+"message":"อัพเดต VPNKUB VPN\nhttps://play.google.com/store/apps/details?id= vpnkub.pro.net",
+   "title":"โปรดทำการอัพเดตแอป ",
+   "link":"play.google.com/store/apps/details?id=vpnkub.pro.net",
+   "version": 2
+}
