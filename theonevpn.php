03ca5b370d024cb52cfbb53ca67553d6dd6dccf4
jame10000lnw <38467489+jame10000lnw@users.noreply.github.com>
2023-06-18 10:08:13 +0700

    theonevpn.php

--- theonevpn.php
+++ theonevpn.php
@@ -1,4 +1,4 @@
-{"Version":"7.8","Url":"https://vpnkub.online/theonevpn.zip","Text":"📢 อัพเดท วันที่ 18 มิถุนายน 2566
+{"Version":"7.9","Url":"https://vpnkub.online/config/theonevpn.zip","Text":"📢 อัพเดท วันที่ 18 มิถุนายน 2566
 
 📢📢📢 ประกาศด่วน 📢📢📢
 เซิร์ฟเวอร์ THEONEVPN จะปิดให้บริการเร็วๆนี้
