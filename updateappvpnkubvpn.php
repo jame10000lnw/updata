d9888ca86c1bae8cf2363148d667dde56ddd6c0e
jame10000lnw <38467489+jame10000lnw@users.noreply.github.com>
2023-09-02 14:17:18 +0700

    Delete updateappvpnkubvpn.php

--- updateappvpnkubvpn.php
+++
@@ -1,7 +0,0 @@
-{
-  
-"message":"อัพเดต VPNKUB VPN\nhttps://play.google.com/store/apps/details?id= vpnkub.pro.net",
-   "title":"โปรดทำการอัพเดตแอป ",
-   "link":"play.google.com/store/apps/details?id=vpnkub.pro.net",
-   "version": 2
-}
