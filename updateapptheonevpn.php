fcd343914c5eb760983f45a24f79d22c7580a45d
jame10000lnw <38467489+jame10000lnw@users.noreply.github.com>
2023-09-02 14:17:55 +0700

    Delete updateapptheonevpn.php

--- updateapptheonevpn.php
+++
@@ -1,7 +0,0 @@
-{
-  
-"message":"อัพเดต THEONE VPN VPN\nhttps://play.google.com/store/apps/details?id= com.nmto.vpn",
-   "title":"โปรดทำการอัพเดตแอป ",
-   "link":"play.google.com/store/apps/details?id=com.nmto.vpn",
-   "version": 8
-}
