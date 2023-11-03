5f25120343474095988862edae1a6abcf8c5aa5c
jame10000lnw <38467489+jame10000lnw@users.noreply.github.com>
2023-11-03 17:52:33 +0700

    Create t.sh

---
+++ t.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# go to root
+cd
+
+# Install Squid
+apt-get -y install squid3
+cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/northvar/pritunl/master/conf/squid.conf" 
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
+sed -i s/xxxxxxxxx/$MYIP/g /etc/squid3/squid.conf;
+service squid3 restart
+
+# Enable Firewall
+sudo ufw allow 22,80,81,222,443,8080,9700,60000/tcp
+sudo ufw allow 22,80,81,222,443,8080,9700,60000/udp
+sudo yes | ufw enable
+
+# Change to Time GMT+7
+ln -fs /usr/share/zoneinfo/Asia/Bangkok /etc/localtime
+
+
+
